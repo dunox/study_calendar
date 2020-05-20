eb015be6054497e86ee2f22052d56a613ab6e5c6
dunox <avshendrik@gmail.com>
2020-05-20 13:40:27 +0300

    Updates

---
+++ precache-manifest.fb7578e17d360620cb2a52605ff83f81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f88efb3e69529bd3c46db47f9e9ab75",
+    "url": "/study_calendar/index.html"
+  },
+  {
+    "revision": "22476246bb9fcf5c8445",
+    "url": "/study_calendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92f9ceacad358468b462",
+    "url": "/study_calendar/static/js/2.780adf32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study_calendar/static/js/2.780adf32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22476246bb9fcf5c8445",
+    "url": "/study_calendar/static/js/main.f96e0254.chunk.js"
+  },
+  {
+    "revision": "18d1185106fb2a3ce0d8",
+    "url": "/study_calendar/static/js/runtime-main.2be09a47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/study_calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
